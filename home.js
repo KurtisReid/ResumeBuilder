6ed44292b4da5c40cbd7b3fd81f09e9b5e849585
kreid8 <kreid8@kent.edu>
2020-07-05 13:34:18 -0400

    reads url from webpage, prints to document

--- home.js
+++ home.js
@@ -5,13 +5,14 @@ var url = require('url');
 var adr = 'http://localhost:8080/default.htm?skills=Node.js (1 year), Python (1 year), Git (4 years), SQL (1 year), JavaScript (2 years), JSON and XML (1 year)';
 //Parse the address:
 var q = url.parse(adr, true);
+//const current_url = new URL('http://localhost:8080/default.htm?skills=Node.js (1 year),');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
-}).listen(8080);
-
-let docx = officegen('docx')
+  
+  res.end(url);
+  
+  let docx = officegen('docx')
  
 // Officegen calling this function after finishing to generate the docx document:
 docx.on('finalize', function(written) {
@@ -32,61 +33,20 @@ var uri_dec = decodeURIComponent(q.search)
 var skills = uri_dec.substring('skills='.length+1)
 pObj.addText(skills)
 
+// get access to URLSearchParams object
+//const search_params = current_url.searchParams;
 
-pObj.addText(' with color', { color: '000088' })
-pObj.addText(' and back color.', { color: '00ffff', back: '000088' })
- 
-pObj = docx.createP()
- 
-pObj.addText('Since ')
-pObj.addText('officegen 0.2.12', {
-  back: '00ffff',
-  shdType: 'pct12',
-  shdColor: 'ff0000'
-}) // Use pattern in the background.
-pObj.addText(' you can do ')
-pObj.addText('more cool ', { highlight: true }) // Highlight!
-pObj.addText('stuff!', { highlight: 'darkGreen' }) // Different highlight color.
- 
-pObj = docx.createP()
- 
-pObj.addText('Even add ')
-pObj.addText('external link', { link: 'https://github.com' })
-pObj.addText('!')
- 
-pObj = docx.createP()
- 
-pObj.addText('Bold + underline', { bold: true, underline: true })
- 
-pObj = docx.createP({ align: 'center' })
- 
-pObj.addText('Center this text', {
-  border: 'dotted',
-  borderSize: 12,
-  borderColor: '88CCFF'
-})
- 
-pObj = docx.createP()
-pObj.options.align = 'right'
- 
-pObj.addText('Align this text to the right.')
- 
-pObj = docx.createP()
- 
-pObj.addText('Those two lines are in the same paragraph,')
-pObj.addLineBreak()
-pObj.addText('but they are separated by a line break.')
- 
-docx.putPageBreak()
- 
-pObj = docx.createP()
- 
-pObj.addText('Fonts face only.', { font_face: 'Arial' })
-pObj.addText(' Fonts face and size.', { font_face: 'Arial', font_size: 40 })
- 
-docx.putPageBreak()
- 
-pObj = docx.createP()
+// get url parameters
+//const id = search_params.get('skills');
+
+
+// "123"
+//console.log(id);
+
+
+
+
+
  
 // We can even add images:
 //pObj.addImage('some-image.png')
@@ -94,10 +54,15 @@ pObj = docx.createP()
 // Let's generate the Word document into a file:
  
 let out = fs.createWriteStream('example.docx')
+
+
  
 out.on('error', function(err) {
   console.log(err)
 })
  
-// Async call to generate the output file:
+
 docx.generate(out)
+  
+}).listen(8080);
+
--- test.js
+++ test.js
@@ -1,15 +1,74 @@
+
+const officegen = require('officegen')
+const fs = require('fs')
 var url = require('url');
-var adr = 'http://localhost:8080/default.htm?year=Node.js (1 year), Python (1 year), Git (4 years), SQL (1 year), JavaScript (2 years), JSON and XML (1 year)&month=march';
-//Parse the address:
-var q = url.parse(adr, true);
+var http = require('http');
+var texty = '';
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  var q = url.parse(req.url, true).query;
+  var txt = q.skills;
+  res.write(req.url);
+  texty = req.url;
+  printDoc(txt);
+  res.end();
+}).listen(8080);
 
-/*The parse method returns an object containing url properties*/
-//console.log(q.host);
-//console.log(q.pathname);
-console.log(q.search);
+function printDoc(urlParam) {
+	console.log("function: " + urlParam);
+	let docx = officegen('docx')
+ 
+// Officegen calling this function after finishing to generate the docx document:
+	docx.on('finalize', function(written) {
+  console.log(
+    'Finish to create a Microsoft Word document.'
+  )
+})
+ 
+// Officegen calling this function to report errors:
+docx.on('error', function(err) {
+  console.log(err)
+})
+ 
+// Create a new paragraph:
+let pObj = docx.createP()
+pObj.addText(urlParam);
+//remove junk from url
+//var uri_dec = decodeURIComponent(urlParam.search)
+//var skills = urlParam.substring('skills='.length+1)
+//pObj.addText(skills)
 
-/*The query property returns an object with all the querystring parameters as properties:*/
-var qdata = q.query;
-console.log(qdata.month);
+// get access to URLSearchParams object
+//const search_params = current_url.searchParams;
+
+// get url parameters
+//const id = search_params.get('skills');
 
 
+// "123"
+//console.log(id);
+
+
+
+
+
+ 
+// We can even add images:
+//pObj.addImage('some-image.png')
+ 
+// Let's generate the Word document into a file:
+ 
+let out = fs.createWriteStream('example.docx')
+
+
+ 
+out.on('error', function(err) {
+  console.log(err)
+})
+ 
+// Async call to generate the output file:
+docx.generate(out)
+   // function body
+   // optional return; 
+} 
+console.log(texty);
